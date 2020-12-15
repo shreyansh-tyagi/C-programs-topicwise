c11616c679b3fd24c98f00afa67c346a81d4ac5f
shreyansh-tyagi <sunnytyagi886@gmail.com>
2020-12-15 19:54:05 +0530

    added even odd file

--- even odd program/main.c
+++ even odd program/main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int a[20],n;
+    int a[20],n,b;
     printf("enter the value upto where you want even number");
     scanf("%d",&n);
     for(int i=0;i<=n;i++)
@@ -11,6 +11,17 @@ int main()
     }
     for(int i=0;i<=n;i++)
     {
-        printf(" %d",(a[i]%2==0));
+        printf(" %d",a[i]);
+    }
+    for( int i=0;i<=n;i++)
+    {
+       // printf(" %d",(a[i]%2==0));
+      // b=a[i]%2;
+      if(a[i]%2==0)
+      {
+          printf("%d",a[i]);
+
+      }
+      // printf("%d",b);
     }
 }
