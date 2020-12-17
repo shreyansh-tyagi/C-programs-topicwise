1ae1a44141d766265ee3f5b0bcb150a101b6b7f3
shreyansh-tyagi <sunnytyagi886@gmail.com>
2020-12-17 16:57:45 +0530

    added smoker program

---
+++ smoker program/main.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+void main()
+{
+    int salary,sum_of_salary;
+    char name,smoker;
+    printf("enter your name :\n");
+    scanf("%s",&name); 
+    printf("enter your salary: \n");
+    scanf("%d",&salary);
+    printf("Are you a smoker, if yes then type 'y' otherwise 'n' \n");
+    scanf("%s",&smoker); 
+    switch(smoker)
+    {
+        case 'y':
+        {
+           // printf("your name: %s\n",name);
+            if(salary<10000)
+            {
+            salary=salary-((salary*10)/100);
+            printf("your salary after deduction as you are a smoker: %d\n",salary);
+            }
+            else
+            {
+              if(salary>10000)
+              {
+                  salary=salary-((salary*15)/100);
+                  printf("your salary after deduction as you are a smoker: %d\n",salary);
+              }  /* code */
+            }
+            break;
+            
+        }
+         case 'n':
+        {
+          //  printf("your name: %s\n",name);
+            if(salary<10000)
+            {
+            salary=salary+((salary*10)/100);
+            printf("your salary after increment as you are a smoker: %d\n",salary);
+            }
+            else
+            {
+              if(salary>10000)
+              {
+                  salary=salary+((salary*15)/100);
+                  printf("your salary after increment as you are a smoker: %d\n",salary);
+              }  /* code */
+            }
+            break;
+            
+        }
+    }
+}
