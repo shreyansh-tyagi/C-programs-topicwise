a00ece17732af67f533223bf4b4544977e54c747
shreyansh-tyagi <sunnytyagi886@gmail.com>
2020-12-18 23:22:56 +0530

    modified duplicate program

--- duplicate elelment in array/main.c
+++ duplicate elelment in array/main.c
@@ -1,24 +1,36 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
 int main()
 {
-    int i,a[100],n,j,b;
+    int i,a[100],n,j;
     printf("enter the value of n:");
     scanf("%d",&n);
-    for(i=0;i<n;i++)
+    for(i=0;i<=n-1;i++)
     {
         scanf("%d",&a[i]);
     }
-    for(i=0;i<n;i++)
+    for(i=0;i<=n-1;i++)
     {
-        for(j=i+1;j<n;j++)
+        for(j=i+1;j<=n-1;j++)
         {
             if(a[i]==a[j])
             {
-                printf(" %d",a[i]);
+                printf("%d ",a[i]);
             
             }
+        
         }
+
+        
         
     }
+    
+           /*  if(false)
+            {
+                printf("-1");
+                exit(0);
+            } */
+    
 
 }
