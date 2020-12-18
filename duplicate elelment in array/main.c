aeb375538cf6b4ead6ad66fbfc7fcc124de0606d
shreyansh-tyagi <sunnytyagi886@gmail.com>
2020-12-18 20:24:13 +0530

    modified duplicate element in array

--- duplicate elelment in array/main.c
+++ duplicate elelment in array/main.c
@@ -8,13 +8,13 @@ int main()
     {
         scanf("%d",&a[i]);
     }
-    for(i=1;i<n;i++)
+    for(i=0;i<n;i++)
     {
         for(j=i+1;j<n;j++)
         {
             if(a[i]==a[j])
             {
-                printf("%d",a[i]);
+                printf(" %d",a[i]);
             
             }
         }
