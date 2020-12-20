478ebb96bb67b4dfc8f39f766c5465bd5531b849
shreyansh-tyagi <sunnytyagi886@gmail.com>
2020-12-20 17:52:37 +0530

    modified gets puts

--- gets and puts function/main.c
+++ gets and puts function/main.c
@@ -2,12 +2,8 @@
 void main()
 {
     char a[50];
-   // char b[50],c[50];
     printf("enter your name : ");
     gets(a);
-   // printf("enter your sir name: ");
-   // gets(b);
-   // c=a+b;
     printf("\n your name: ");
     puts(a);
 }
