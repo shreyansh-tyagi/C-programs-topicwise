90d1c9bbc6a10061ea8e5d35f85c14e2a6fadbfe
shreyansh-tyagi <sunnytyagi886@gmail.com>
2020-12-22 15:37:10 +0530

    modified some statement in this program

--- getc and putc program/main.c
+++ getc and putc program/main.c
@@ -6,14 +6,17 @@ void main()
   gets(a);
   printf("your name is :");
   puts(a); 
+  fgets(a,4,stdin);
+  printf("character is : %s",a);
   char t='s',u='n',c;
   printf("enter the any character:");
   c=getchar();  //getchar takes only single chracter as input
   putchar(t);   //putchar gives  only single charcter as output and if we concatenate 
                   //then it give result according to its ASCII value
   putchar(u);
-  putchar(t+u);
-  putchar(c);
+  putchar(t+c);
+  putchar( c);
+  
   
 
 }
