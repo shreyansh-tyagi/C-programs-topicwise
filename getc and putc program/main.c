a1cb5daace173a8c0948b99bf0cd0daac20131b9
shreyansh-tyagi <sunnytyagi886@gmail.com>
2020-12-22 15:02:10 +0530

    getc and putc program has been added

---
+++ getc and putc program/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  /*char a[50];
+  printf("enter your name:");
+  gets(a);
+  printf("your name is :");
+  puts(a); */
+  char t='s',u='n';
+  putchar(t);   //putchar takes only single charcter and if we concatenate 
+                  //then it give result according to its ASCII value
+  putchar(u);
+  
+
+}
