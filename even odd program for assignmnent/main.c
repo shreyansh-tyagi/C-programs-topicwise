1d1b93a9ff5c31d3fd6a1de7117b72d24d2f96e0
shreyansh-tyagi <sunnytyagi886@gmail.com>
2020-12-27 19:14:10 +0530

    even odd program for assignment has been added

---
+++ even odd program for assignmnent/main.c
@@ -0,0 +1,14 @@
+     #include<stdio.h>
+     int main ()
+
+     {
+         int number;
+         printf ("enter a number = ");
+         scanf ("%d", &number);
+         
+         if (number%2==0)
+            printf ("Number is Even ");
+         else
+         printf ("number is Odd ");
+        return 0;
+     } 
