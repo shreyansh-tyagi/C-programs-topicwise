ddac1ef690aeecaa9690942a9e8b9ce883ece5a2
shreyansh-tyagi <sunnytyagi886@gmail.com>
2020-12-28 16:35:21 +0530

    added nested if program

---
+++ nested-if program/main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the value of a,b,c");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a<b)
+    {
+        if(b<c)
+        {
+            printf("we are nested if");
+        }
+        else
+        {
+            printf("we are in inner else");
+        }
+        
+    }
+    else
+    {
+        printf("we are in outer else");
+    }
+}
