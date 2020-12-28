a5872ced76258411c4916de0cd35e2e05018abbf
shreyansh-tyagi <sunnytyagi886@gmail.com>
2020-12-28 17:26:13 +0530

    added nested if program

--- nested if program/main.c
+++ nested if program/main.c
@@ -12,7 +12,7 @@ int main() {
         // Nested - if statement 
         // Will only be executed if statement above 
         // is true 
-        if (i > 12) 
+        if (i < 12) 
             printf("i is smaller than 12 too\n"); 
         else
             printf("i is greater than 15"); 
