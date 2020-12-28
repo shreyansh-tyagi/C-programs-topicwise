e72fcdf57aa2cacfbbd3a834311da0423022fcda
shreyansh-tyagi <sunnytyagi886@gmail.com>
2020-12-28 18:49:05 +0530

    added linear search program

--- linear search/main.c
+++ linear search/main.c
@@ -1,16 +1,5 @@
 #include <stdio.h> 
-  
-void findElement(int arr[], int size, int key)  
-{  
-    // loop to traverse array and search for key  
-    for (int i = 0; i < size; i++) {  
-        if (arr[i] == key) {  
-            printf("Element found at position: %d", (i + 1));  
-            break; 
-        }  
-    }  
-}  
-  
+void findElement(int[], int, int);  
 int main() { 
     int arr[] = { 1, 2, 3, 4, 5, 6 };  
       
@@ -22,6 +11,15 @@ int main() {
   
     // Calling function to find the key  
     findElement(arr, n, key);  
-  
-    return 0; 
 } 
+void findElement(int arr[], int size, int key)  
+{  
+    // loop to traverse array and search for key  
+    for (int i = 0; i < size; i++) {  
+        if (arr[i] == key) {  
+            printf("Element found at position: %d", (i + 1));  
+            break; 
+        }  
+    }  
+}  
+  
