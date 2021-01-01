89694784ad32f00f8b9b35562ffeed0641dbb473
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-01 15:39:07 +0530

    function program is added

--- function(with return type and with argument)/main.c
+++ function(with return type and with argument)/main.c
@@ -4,15 +4,24 @@
 int add(int,int);       //function declaration must end with semicolon and should be above main function
 int main()
 {
-    int a,b,sum=0;
+    int a,b,sum=0,s=0;
     printf("enter the value of a and b: ");
     scanf("%d%d",&a,&b);
     sum=add(a,b);        //function call must end with semicolon and should be inside main body
     printf("sum:%d",sum);
+    s=sub(a,b);
+    printf("sub:%d",sub);
 }
 int add(int x, int y)    //function defination should not end with semicolon and should outside main body (upward or downword main body)
 {
     int res;
     res=x+y;
     return res;
+}
+int sub(int x,int y)
+{
+    int res;
+    res=x-y;
+    return res;
+
 }
