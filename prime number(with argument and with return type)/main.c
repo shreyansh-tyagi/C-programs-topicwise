f16dc3aaaa82382cf5322f3eb4b04b68b4cbe734
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-06 00:14:41 +0530

    prime number(with argument and with return type) has been added

---
+++ prime number(with argument and with return type)/main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int primenumber(int);
+int main()
+{
+    int n,flag;
+    printf("enter the value\n:");
+    scanf("%d",&n);
+    flag=primenumber(n);
+    if(flag>0)
+    printf("not a prime number\n");
+    else
+    {
+        printf("prime number");
+    }
+    
+}
+int primenumber(int a)
+{
+    int flag=0;
+    for(int i=2;i<a;i++)
+    {
+        if(a%i==0)
+        flag++;
+
+    }
+    return (flag);
+}
