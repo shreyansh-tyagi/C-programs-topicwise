effd46c8ce835f802dfa3710c2ae8ce9b9e20b7f
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-08 18:46:46 +0530

    search,delete,sort,insert,insert of another array into array

--- searching of array/main.c
+++ searching of array/main.c
@@ -1,22 +1,24 @@
 #include<stdio.h>
+void deletion(int[],int);
 void main()
 {
 int i,a[100],n,b,c=0,d=0,loc,key;
 char s;
 printf("enter how many element you want into the array: ");
 scanf("%d",&n);
-//insertion into the array
+//insertion into the array from user
 for(i=1;i<=n;i++)
 {
     printf("enter the value into array: ");
     scanf("%d",&a[i]);
 }
+//printing array element
 printf("array element are:\n");
 for(i=1;i<=n;i++)
 {
     printf(" %3d",a[i]);
 }
-
+//searching element inside array
 printf("\nenter the element you want to search: ");
 scanf("%d",&b);
 for(i=1;i<=n;i++)
@@ -29,7 +31,7 @@ for(i=1;i<=n;i++)
 }
 if(d==1)
 {
-    printf("\nit is present at position: %d ",c);
+    printf("\nit is present at position: %d and the value is :%d",c,a[c]);
 }
 else{
     printf("\nsorry bhaiyo ye toh nai mil payega\n");
@@ -63,4 +65,31 @@ switch(s)
         printf("\nchla ja b..k\n");
     }
 }
+deletion(a,n);
+
+}
+void deletion(int a[],int n)
+{
+    int loc,i;
+    char s;
+    printf("do you want to delete any element from the array:\n type 'y' or 'n' : ");
+    scanf("%s",&s);
+    if(s=='y')
+    {
+        printf("\nenter the location at which you want to delete the element: ");
+        scanf("%d",&loc);
+        a[loc]=a[loc+1];
+    }
+    switch(s)
+    {
+        case 'y':
+        {
+            for(i=loc+1;i<=n;i++)
+            {
+                a[i]=a[i+1];
+            }
+            
+        }
+    }
+
 }
