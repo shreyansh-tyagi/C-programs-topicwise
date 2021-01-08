e256e700bcfe84bd96f22a80c4a7c2864cc81a4a
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-08 17:02:17 +0530

    searching program has been added

--- searching of array/main.c
+++ searching of array/main.c
@@ -3,21 +3,21 @@ void main()
 {
 int i,a[100],n,b,c=0,d=0,loc,key;
 char s;
-printf("enter how many element you want into the array:\n");
+printf("enter how many element you want into the array: ");
 scanf("%d",&n);
 //insertion into the array
 for(i=1;i<=n;i++)
 {
-    printf("enter the value into array:\n");
+    printf("enter the value into array: ");
     scanf("%d",&a[i]);
 }
-printf("printing the array element:\n");
+printf("array element are:\n");
 for(i=1;i<=n;i++)
 {
-    printf("%3d",a[i]);
+    printf(" %3d",a[i]);
 }
 
-printf("\nenter the element you want to search\n");
+printf("\nenter the element you want to search: ");
 scanf("%d",&b);
 for(i=1;i<=n;i++)
 {
@@ -34,22 +34,25 @@ if(d==1)
 else{
     printf("\nsorry bhaiyo ye toh nai mil payega\n");
 }
-printf("\nDo you want to insert any element into the array:\nType 'y' or 'n :");
+printf("\nDo you want to insert any element into the array...\nType 'y' or 'n : ");
 scanf("%s",&s);
-printf("\nIf 'yes' then enter the location you want to insert the element:\n");
-scanf("%d",&loc);
-printf("enter the new key element you want to insert:\n");
+if(s=='y')
+{
+printf("If 'yes' Enter the new key element you want to insert:\n");
 scanf("%d",&key);
+printf("enter the location you want to insert the element:\n");
+scanf("%d",&loc);
+}
 switch(s)
 {
     case'y':
     {
-        for(i=n;i<=loc;i--)
+        for(i=n;i>=loc;i--)
         {
             a[i+1]=a[i];
         }
         a[loc]=key;
-        for(i=1;i<=n;i++)
+        for(i=1;i<=n+1;i++)
         {
             printf(" %3d",a[i]);
         }
