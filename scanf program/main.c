9b409ba6ccf080400aee616f8f152727b85ed33a
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-09 18:41:23 +0530

    gets and puts program has been added

--- scanf program/main.c
+++ scanf program/main.c
@@ -4,8 +4,8 @@ void main()
     int a,b;
     printf("enter the value of a and b: ");
     scanf("%d%d",&a,&b);
-    printf("%d+%d=%d",a,b,a+b);
-    printf("%d-%d=%d",a,b,a-b);
+    printf("%d+%d=%d\n",a,b,a+b);
+    printf("%d-%d=%d\n",a,b,a-b);
     
-    printf("%d*%d=%d",a,b,a*b);
+    printf("%d*%d=%d\n",a,b,a*b);
 }
