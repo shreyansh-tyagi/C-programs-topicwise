3aaa1a04cf5ca877b51afecece3fe8c37bdebb6c
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-13 16:33:01 +0530

    even number from one array to another has been added

---
+++ even number from one array to another/main.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+    int a[100],b[100],n,i,c=0;
+    printf("enter the value of n: ");
+    scanf("%d",&n);
+    printf("\nenter the %d value into the array: ",n);
+    for(i=0;i<n;i++)
+    {
+        scanf(" %d",&a[i]);
+    }
+    printf("\nfirst array: ");
+    for(i=0;i<n;i++)
+    {
+        printf(" %d",a[i]);
+    }
+    while(c<n)
+    {
+        if(a[c]%2==0)
+        {
+            b[c]=a[c];
+        }
+        else
+        {
+            b[c]=0;
+        }
+        c++;
+    }
+    printf("\nsecond array that contain only even array: ");
+    for(i=0;i<n;i++)
+    {
+        printf(" %d",b[i]);
+    }
+}
