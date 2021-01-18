07d68c0de1d3ba9d22b4ab9ae022009051d92ad3
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-18 17:00:07 +0530

    auto and static storage program has been added

---
+++ auto and static storage class/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void increment();
+void main()
+{
+    increment();
+    increment();
+    increment();
+}
+void increment()
+{
+    auto int a=0;
+    a+=1;
+    printf("a=%d\n",a);
+
+}
