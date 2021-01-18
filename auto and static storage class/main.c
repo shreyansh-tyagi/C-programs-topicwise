f3393e1fb2ac3610fd211719d103ee5f6fe01e40
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-18 20:15:11 +0530

    auto and static storage class has been added

--- auto and static storage class/main.c
+++ auto and static storage class/main.c
@@ -1,15 +1,26 @@
 #include<stdio.h>
 void auto_class();
+void static_class();
 void main()
 {
     auto_class();
     auto_class();
     auto_class();
+    static_class();
+    static_class();
+    static_class();
 } 
 void auto_class()
 {
     auto int a=0;
     a+=1;
     printf("a=%d\n",a);
+
+}
+void static_class()
+{
+    static int b=0;
+    b+=1;
+    printf("b=%d\n",b);
 
 }
