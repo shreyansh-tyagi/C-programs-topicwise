433d5cb6be4fc15a5c7576a42bc18091b954f87e
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-19 12:50:23 +0530

    to check whether array contain garbage value

---
+++ to check whether array contain garbage value/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a[10]={1};
+   /* for(int i =0;i<=5;i++)
+    {
+        scanf("%d",&a[i]);
+    }*/
+    for(int i=0;i<=5;i++)
+    {
+        printf(" %d",a[i]);
+    }
+}
