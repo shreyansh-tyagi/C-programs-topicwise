e1f69a8a223473ca5abeb5442761b1c6f2d45334
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-25 20:25:55 +0530

    muliplication of array program has been added

--- mutiplication of array/main.c
+++ mutiplication of array/main.c
@@ -2,5 +2,31 @@
 void main()
 {
     int a[10][10],b[10][10],c[10][10],i,j,k,r1,c1,r2,c2;
-    printf("enter the row ")
+    printf("enter the row of first array: ");
+    scanf("%d",&r1);
+    printf("enter the coloumn of first array: ");
+    scanf("%d",&c1);
+    printf("enter the row of second arrya: ");
+    scanf("%d",&r2);
+    printf("enter the coloumn of second arrya: ");
+    scanf("%d",&c2);
+    if(c1==r2)
+    {
+        for(i=0;i<r1;i++)
+        {
+            for(j=0;j<c1;j++)
+            {
+                scanf("%d",&a[i][j]);
+            }
+        }
+        printf("first array: ");
+        for(i=0;i<r1;i++)
+        {
+            for(j=0;j<c1;j++)
+            {
+                printf("%d",a[i][j]);
+            }
+            printf("\n");
+        }
+    }
 }
