87d215c5bd4f3bebc6281dc4b4c656dc2bad74c6
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-28 19:02:08 +0530

    pointer ot pointer program has been added

---
+++ pointer to pointer/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ 
+int main () {
+
+   int  var;
+   int  *ptr;
+   int  **pptr;
+
+   var = 3000;
+
+   /* take the address of var */
+   ptr = &var;
+
+   /* take the address of ptr using address of operator & */
+   pptr = &ptr;
+
+   /* take the value using pptr */
+   printf("Value of var = %d\n", var );
+   printf("value of ptr=%d\n",ptr);
+   printf("Value available at *ptr = %d\n", *ptr );
+   printf("value of pptr=%d\n",pptr);
+   printf("Value available at **pptr = %d\n", **pptr);
+
+   return 0;
+}
