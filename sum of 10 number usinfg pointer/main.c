a03376101c8d6dd551c01a5076fb86d5b99b495c
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-28 15:23:45 +0530

    sum of 10 number using pointer arithematics has been added

--- sum of 10 number usinfg pointer/main.c
+++ sum of 10 number usinfg pointer/main.c
@@ -2,15 +2,16 @@
 void main()
 {
     int a[10]={1,1,1,1,1,1,1,1,1,1};
-    int *ptr[10];
+    int *ptr;
     int i,sum=0;
-    for(i=0;i<=9;i++)
+    ptr=a;
+    /*for(i=0;i<=9;i++)
     {
         ptr[i]=&a[i];
-    }
+    }*/
     for(i=0;i<=9;i++)
     {
-        sum+=*(*(ptr+i));
+        sum+=*(ptr+i);
     }
     printf("sum=%d",sum);
 }
