63f9a666e1cf9e7e133f188bb74d5ca7bd007690
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-29 00:20:44 +0530

    structure to array with pointer using function has been added

--- structure to array with pointer using function/main.c
+++ structure to array with pointer using function/main.c
@@ -9,13 +9,20 @@ struct std
 int main()
 {
     struct std e[10],*ptr;
-    int n,i;
+    int n,i,j;
     ptr=e;
     printf("enter the value of n:");
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         printf("enter the student id: ");
-        scanf("%d",(ptr+i))
+        scanf("%d",&(ptr+i)->id);
+        printf("enter the student name: ");
+        scanf("%s",(ptr+i)->name);
+        for(j=0;j<3;j++)
+        {
+            printf("enter the mark %d subject: ",j+1);
+            scanf("%f",&(ptr+i)->marks[j])
+        }
     }
 }
