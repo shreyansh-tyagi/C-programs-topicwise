58eabe5ce99815139571da7b6bef1ccd82bca94b
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-29 19:19:34 +0530

    function to pointer program has been added

--- function pointer/main.c
+++ function pointer/main.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
-int swap(int *,int *);  //function declaration that have two integer pointer argument and returing a integer value 
+void swap(int *,int *);  //function declaration that have two integer pointer argument and returing a integer value 
 void main()
 {
-    int a,b,r1;
-    int (*ptr)(int *,int*); //function pointer that can point to the function that have two integer 
+    int a,b;
+    void (*ptr)(int *,int*); //function pointer that can point to the function that have two integer 
                              //pointer argument and return a integer value 
     printf("enter the value of a and b: \n");
     scanf("%d%d",&a,&b);
     printf("before swap: a=%d\nb=%d",a,b);
     ptr=&swap; //passing the address of swap function block to function pointer ptr
-    r1=ptr(&a,&b);
+    ptr(&a,&b);
     printf("after swap: a=%d\nb=%d",a,b);
 }
-int swap(int *x,int *y)
+void swap(int *x,int *y)
 {
     int temp=0;
     temp=*x;
