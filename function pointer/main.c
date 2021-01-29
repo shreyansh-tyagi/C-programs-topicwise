f2204a8d26901b979f4475bfbcf8d94a3fe3290a
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-29 19:15:58 +0530

    function to pointer program has been added

--- function pointer/main.c
+++ function pointer/main.c
@@ -8,6 +8,14 @@ void main()
     printf("enter the value of a and b: \n");
     scanf("%d%d",&a,&b);
     ptr=&swap; //passing the address of swap function block to function pointer ptr
-    r1=ptr(a,b);
+    r1=ptr(&a,&b);
     printf("after swap: a=%d\nb=%d",a,b);
+}
+int swap(int *x,int *y)
+{
+    int temp=0;
+    temp=*x;
+    *x=*y;   //call by reference process
+    *y=temp;
+    printf("after swap: x=%d\ny=%d",x,y);
 }
