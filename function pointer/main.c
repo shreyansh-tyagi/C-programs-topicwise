d1eaa177f585cfdf7645e3fd071596ee08707416
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-01-29 19:08:35 +0530

    structure to array with pointer using function has been added

--- function pointer/main.c
+++ function pointer/main.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
-int swap(int *,int *); 
+int swap(int *,int *);  //function declaration that have two integer pointer argument and returing a integer value 
 void main()
 {
-    int a,b;
-    int (*ptr)(int *,int*);
+    int a,b,r1;
+    int (*ptr)(int *,int*); //function pointer that can point to the function that have two integer 
+                             //pointer argument and return a integer value 
     printf("enter the value of a and b: \n");
     scanf("%d%d",&a,&b);
-
+    ptr=&swap; //passing the address of swap function block to function pointer ptr
+    r1=ptr(a,b);
+    printf("after swap: a=%d\nb=%d",a,b);
 }
