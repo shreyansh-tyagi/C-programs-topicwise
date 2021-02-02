4306ac5a1c9666bdb0983ddf2b1e857dd5459abd
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-02 19:45:37 +0530

    string function has been added

--- string length function/main.c
+++ string length function/main.c
@@ -3,6 +3,15 @@
 void main()
 {
     char ch[]="shreyansh tyagi";
+    char ch2[]=" sunny tyagi";
+    char *val;
+    char a;
+    a='s';
     printf("%s\n",ch);
     printf("%d",strlen(ch));
+    strcat(ch,ch2);
+    printf("\nstring concatenate: %s\n",ch);
+    val=strrchr(ch,a);
+    printf("val=%s",val);
+
 }
