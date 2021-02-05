8c32518887e8a2b569482162d2845b2bb8da7af9
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-05 14:51:56 +0530

    return struture using function has been added

---
+++ return structure using function/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+struct student
+{
+    char name[50];
+    int age;
+};
+
+// function prototype
+struct student abc();
+
+int main()
+{
+    struct student s;
+
+    s = abc();
+
+    printf("\nDisplaying information\n");
+    printf("Name: %s", s.name);
+    printf("\nRoll: %d", s.age);
+    
+    return 0;
+}
+struct student abc() 
+{
+  struct student s;
+
+  printf("Enter name: ");
+  scanf ("%[^\n]%*c", s.name);
+
+  printf("Enter age: ");
+  scanf("%d", &s.age);
+  
+  return s1;
+}	
