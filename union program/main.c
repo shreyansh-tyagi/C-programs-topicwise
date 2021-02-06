2cdb9f44789526392e6fc8ac00eacc22739beef2
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-06 21:27:59 +0530

    union program has been added

---
+++ union program/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+union Job {
+   float salary;
+   int workerNo;
+} j;
+
+int main() {
+   j.salary = 12.3;
+
+   // when j.workerNo is assigned a value,
+   // j.salary will no longer hold 12.3
+   j.workerNo = 100;
+
+   printf("Salary = %.1f\n", j.salary);
+   printf("Number of workers = %d", j.workerNo);
+   return 0;
+}
