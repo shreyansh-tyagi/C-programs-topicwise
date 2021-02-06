58f2562b847f8d1293717df43a811291228e5e1f
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-06 22:59:40 +0530

    union and structure size difference program

--- union and structure size difference/main.c
+++ union and structure size difference/main.c
@@ -2,16 +2,17 @@
 union unionJob
 {
    //defining a union
-   char name[32]; //
-   float salary;
-   int workerNo;
-} uJob;
+   char name[32]; //char =32 byte 
+   float salary; //float= 4 byte
+   int workerNo; //int =4 byte 
+} uJob; //but as it is union it will take the size of its largest element which is char = 32 byte 
 
 struct structJob
 {
-   char name[32];
-   float salary;
-   int workerNo;
+   char name[32]; //char =32 byte 
+   float salary; // float =4 byte 
+   int workerNo; //int = 4 byte //structure will take the size of all the element at once 
+
 } sJob;
 
 int main()
