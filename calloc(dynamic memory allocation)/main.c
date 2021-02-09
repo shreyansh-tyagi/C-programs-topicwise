95d989b7aabcab91d177a64f446a1afc823d359a
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-09 19:22:35 +0530

    calloc method program has been added

--- calloc(dynamic memory allocation)/main.c
+++ calloc(dynamic memory allocation)/main.c
@@ -5,18 +5,19 @@
 
 int main()
 {
-    int n,i,*arr;
+    int n,i,*arr,a;
     printf("enter the value of n: ");
     scanf("%d",&n);
     arr=(int*)calloc(n,sizeof(int));  //calloc function will take two argument first is length of array(5 or anything) and size of datatype(int)
 // hence int varies from 2 byte to 4 byte therefore for 32 and 64 bit compiler int take 4 byte so total of 5*4=20 byte will be allocates.
-    printf("size of int: %d",sizeof(int));
+    a=n*sizeof(int);
+    printf("total size : %d",a);
     printf("\nenter the array elemnts: ");
-    for(i=0;i<=n;i++)
+    for(i=0;i<n;i++)
     {
         scanf("%d",arr+i);
     }
-    for(i=0;i<=n;i++)
+    for(i=0;i<n;i++)
     {
         printf(" %d",*(arr+i));
     }
