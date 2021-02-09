a504118c2b511789e2a4e813c62e7a2f80e93886
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-09 20:14:47 +0530

    reverse of array using malloc method

--- reverse of array using malloc/main.c
+++ reverse of array using malloc/main.c
@@ -4,21 +4,23 @@
 int main()
 {
     int num, *arr, i,temp=0;
+    printf("enter the size of array: ");
     scanf("%d", &num);
     arr = (int*) malloc(num * sizeof(int));
     for(i = 0; i < num; i++) {
+        printf("enter the value into array: ");
         scanf("%d", arr + i);
     }
 
 
     /* Write the logic to reverse the array. */
 
-    for (i = 0; i < num / 2; i++) {
+   /* for (i = 0; i < num / 2; i++) {
         temp = (int) *(arr + num - i - 1);
         *(arr + num - i - 1) = *(arr + i);
         *(arr + i) = temp;
-    }
-    for(i = 0; i < num; i++)
+    } */
+    for(i = num-1; i>=0; i--)
         printf("%d ", *(arr + i));
-    return 0;
+   // return 0;
 }
