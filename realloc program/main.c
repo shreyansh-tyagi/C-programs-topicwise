a434aec8311c90920a96711e4480189ccf3dc298
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-10 00:14:23 +0530

    reallov function in dynamic memory allocation has been added

--- realloc program/main.c
+++ realloc program/main.c
@@ -11,14 +11,23 @@ struct std{
         float marks;
     }*m;
 }*s;
-void structure(struct std *,int);
-void sum_of_marks(struct std *,int);
+void structure(int,int);
+void sum_of_marks(int,int);
 void main()
 {
-    int n;
+    int n,m;
     void (*structure_function_pointer)(struct std *,int);
     printf("\nenter the value of n: ");
     scanf("%d",&n);
+    printf("enter the value of m: ");
+    scanf("%d",&m);
     structure_function_pointer=&structure;
-    structure_function_pointer()
+    structure_function_pointer(n,m);
+    structure_function_pointer=&sum_of_marks;
+    structure_function_pointer(n,m);
+
+}
+void structure(int n,int m)
+{
+    s=(struct std*)malloc(n*sizeof(struct std));
 }
