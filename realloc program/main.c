508c2272c4cc6afaf41d734b33c348155aeff27f
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-10 01:04:29 +0530

    reallov function in dynamic memory allocation has been added

--- realloc program/main.c
+++ realloc program/main.c
@@ -31,20 +31,69 @@ void structure(int n,int m)
 {
     int i,j;
     s=(struct std*)malloc(n*sizeof(struct std));
+    if(s==NULL)
+    {
+        printf("out of memory");
+    }
+    else{
     for(i=0;i<n;i++)
     {
     printf("\nenter id: ");
     scanf("%d",&(s+i)->id);
     printf("\nenter age: ");
     scanf("%d",&(s+i)->age);
-    printf("enter name: ");
+    printf("\nenter name: ");
     fgets((s+i)->name,sizeof(s->name),stdin);
-    printf("enter the day-of-birth: ");
+    printf("\nenter the day-of-birth: ");
     scanf("%d%d%d",&(s+i)->d.day,&(s+i)->d.month,&(s+i)->d.year);
     for(j=0;j<m;j++)
     {
-        printf("enter the marks of %d subject: ",j+1);
+        printf("\nenter the marks of %d subject: ",j+1);
         scanf("%f",&(s+i)->ma.marks[j]);
     }
+    }
+    for(i=0;i<n;i++)
+    {
+    printf("\nid: %d",(s+i)->id);
+    printf("\nage: %d",(s+i)->age);
+    puts((s+i)->name);
+    printf("\nday-of-birth: %d%d%d",(s+i)->d.day,(s+i)->d.month,(s+i)->d.year);
+    for(j=0;j<m;j++)
+    {
+        printf("\nmarks of %d subject: %f",j+1,(s+i)->ma.marks[j]);
+    }
+    }
+    
+    }
+    n++;
+    s=realloc(s,n*sizeof(struct std));
+    for(i=0;i<n;i++)
+    {
+    printf("\nenter id: ");
+    scanf("%d",&(s+i)->id);
+    printf("\nenter age: ");
+    scanf("%d",&(s+i)->age);
+    printf("\nenter name: ");
+    fgets((s+i)->name,sizeof(s->name),stdin);
+    printf("\nenter the day-of-birth: ");
+    scanf("%d%d%d",&(s+i)->d.day,&(s+i)->d.month,&(s+i)->d.year);
+    for(j=0;j<m;j++)
+    {
+        printf("\nenter the marks of %d subject: ",j+1);
+        scanf("%f",&(s+i)->ma.marks[j]);
+    }
+    }
+    for(i=0;i<n;i++)
+    {
+    printf("\nid: %d",(s+i)->id);
+    printf("\nage: %d",(s+i)->age);
+    puts((s+i)->name);
+    printf("\nday-of-birth: %d%d%d",(s+i)->d.day,(s+i)->d.month,(s+i)->d.year);
+    for(j=0;j<m;j++)
+    {
+        printf("\nmarks of %d subject: %f",j+1,(s+i)->ma.marks[j]);
+    }
+    }
+    
     }
 }
