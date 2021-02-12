98a8554c93e498a14b0faba9a6ea77ecbfb70582
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-12 15:34:15 +0530

    file handling process in c

---
+++ file handling/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void main()
+{
+    FILE *ptr=NULL;
+    char s[50];
+    ptr=fopen("mytext.txt","r");
+    fscanf(ptr,"%s",s);
+    printf("after reading file from mytext we get: %s",s);   
+}
