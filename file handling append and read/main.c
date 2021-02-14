23244f550e2b626f2edffe9c9031d1336c413c8b
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-14 21:10:52 +0530

    file handling read,write,append method

--- file handling append and read/main.c
+++ file handling append and read/main.c
@@ -6,9 +6,13 @@ void main()
     FILE *ptr=NULL;
     char string[50];
     char s[50]="this is the text i gonna append in test.txt file";
+    char s1[100]="this is the text i gonna write in testforwrite file";
     ptr=fopen("D://c programs//file handling write and print//test.txt","r");
     fscanf(ptr,"%s",string);
-    printf("the file is: %s",string);
+    printf("the file is:   %s",string);
     ptr=fopen("D://c programs//file handling write and print//test.txt","a");
     fprintf(ptr,"%s",s);
+    ptr=fopen("D://c programs//file handling write and print//testforwrite.txt","w");
+    fprintf(ptr,"%s",s1);
+    fclose(ptr);
 }
