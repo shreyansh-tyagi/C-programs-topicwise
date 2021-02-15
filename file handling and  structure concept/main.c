ec2370ba5394fc688795d4cf8e53a6ee72543b26
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-16 00:31:30 +0530

    linear search program has been added

--- file handling and  structure concept/main.c
+++ file handling and  structure concept/main.c
@@ -14,14 +14,14 @@ struct std{
 void structure(struct std *,int,int);
 void main()
 {
-    int n,m,i,j,a;
-    void (*structure_function_pointer)(sturct std *,int,int);
+    int n,m,i,j;
+    void (*structure_function_pointer)(struct std *,int,int);
     printf("\nenter the value of n: ");
     scanf("%d",&n);
     printf("enter the value of m: ");
     scanf("%d",&m);
     structure_function_pointer=&structure;
-    s=(struct std*)malloc(n*sizeof(struct std));
+    s=(struct std*)calloc(n,sizeof(struct std));
     if(s==NULL)
     {
         printf("out of memory");
@@ -43,11 +43,12 @@ void main()
         scanf("%f",&(s+i)->ma.marks[j]);
     }
     }
-    structure_function_pointer(n,m);
+    structure_function_pointer(s,n,m);
 }
-void structure(int n,int m)
+}
+void structure(struct std *s,int n,int m)
 {
-   
+   int i,j;
     for(i=0;i<n;i++)
     {
     printf("\nid: %d",(s+i)->id);
@@ -59,42 +60,4 @@ void structure(int n,int m)
         printf("\nmarks of %d subject: %f",j+1,(s+i)->ma.marks[j]);
     }
     }
-    a=n*sizeof(e);
-    printf("\n\nsize of structure after malloc: %d",a);
-    }
-    n++;
-    s=realloc(s,n*sizeof(struct std));
-    for(i=1;i<n;i++)
-    {
-    printf("\nenter id: ");
-    scanf("%d",&(s+i)->id);
-    printf("\nenter age: ");
-    scanf("%d",&(s+i)->age);
-    printf("\nenter name: ");
-    scanf("%s",(s+i)->name);
-    printf("\nenter the day-of-birth: ");
-    scanf("%d%d%d",&(s+i)->d.day,&(s+i)->d.month,&(s+i)->d.year);
-    for(j=0;j<m;j++)
-    {
-        printf("\nenter the marks of %d subject: ",j+1);
-        scanf("%f",&(s+i)->ma.marks[j]);
-    }
-    }
-    for(i=0;i<n;i++)
-    {
-    printf("\nid: %d",(s+i)->id);
-    printf("\nage: %d",(s+i)->age);
-    printf("\nname: %s",(s+i)->name);
-    printf("\nday-of-birth: %d-%d-%d",(s+i)->d.day,(s+i)->d.month,(s+i)->d.year);
-    for(j=0;j<m;j++)
-    {
-        printf("\nmarks of %d subject: %f\n",j+1,(s+i)->ma.marks[j]);
-    }
-    }
-    
-    a=n*sizeof(e);
-    printf("\n\nsize of structure after realloc: %d",a);
-    free(s);
-    printf("\n\nid: %d",s->id);
-    
-    }
+}
