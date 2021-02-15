9388630c00081d5f76b633083d5631fa1e25ebed
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-15 23:43:24 +0530

    file handling and structure concepts

--- file handling and  structure concept/main.c
+++ file handling and  structure concept/main.c
@@ -7,13 +7,18 @@ struct std {
     struct date_of_birth{
         int day,month,year;
     }d;
+    struct marks{
     float marks[100];
-}
+    }ma;
+}*s;
 void structure_function(struct std *,int);
 void sum_of_marks(struct std *,int);
 void main()
 {
-    int n,m
-    
+    int n,m;
+    printf("\nEnter the number of student: ");
+    scanf("%d",&n);
+    printf("\nEnter the number of subjects: ");
+    scanf("%d",&m);
     printf();
 }
