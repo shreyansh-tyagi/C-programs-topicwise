45eec2f88447a8c5138bf5f1caf17d992f7bcbff
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-16 00:18:35 +0530

    linear search program has been added

--- file handling and  structure concept/main.c
+++ file handling and  structure concept/main.c
@@ -1,29 +1,100 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-struct std {
+struct std{
     int id,age;
-    char name[100];
+    char name[50];
     struct date_of_birth{
         int day,month,year;
     }d;
     struct marks{
-    float marks[20];
+        float marks[50];
     }ma;
-}*s;
-void structure_function(struct std *,int);
-void sum_of_marks(struct std *,int);
+}*s,e;
+void structure(struct std *,int,int);
 void main()
 {
-    int n,m,i,j;
-    void (*structure_function_pointer)(struct std *,int);
-    structure_function_pointer=&structure_function;
-    void (*sum_of_marks_function_pointer)(struct std * ,int);
-    sum_of_marks_function_pointer=&sum_of_marks;
-    s=(struct std *)calloc(n,sizeof(struct std));
-    printf("\nEnter the number of student: ");
+    int n,m,i,j,a;
+    void (*structure_function_pointer)(sturct std *,int,int);
+    printf("\nenter the value of n: ");
     scanf("%d",&n);
-    printf("\nEnter the number of subjects: ");
+    printf("enter the value of m: ");
     scanf("%d",&m);
-    printf();
+    structure_function_pointer=&structure;
+    s=(struct std*)malloc(n*sizeof(struct std));
+    if(s==NULL)
+    {
+        printf("out of memory");
+    }
+    else{
+    for(i=0;i<n;i++)
+    {
+    printf("\nenter id: ");
+    scanf("%d",&(s+i)->id);
+    printf("\nenter age: ");
+    scanf("%d",&(s+i)->age);
+    printf("\nenter name: ");
+    scanf("%s",(s+i)->name);
+    printf("\nenter the day-of-birth: ");
+    scanf("%d%d%d",&(s+i)->d.day,&(s+i)->d.month,&(s+i)->d.year);
+    for(j=0;j<m;j++)
+    {
+        printf("\nenter the marks of %d subject: ",j+1);
+        scanf("%f",&(s+i)->ma.marks[j]);
+    }
+    }
+    structure_function_pointer(n,m);
 }
+void structure(int n,int m)
+{
+   
+    for(i=0;i<n;i++)
+    {
+    printf("\nid: %d",(s+i)->id);
+    printf("\nage: %d",(s+i)->age);
+    printf("\nname: %s",(s+i)->name);
+    printf("\nday-of-birth: %d-%d-%d",(s+i)->d.day,(s+i)->d.month,(s+i)->d.year);
+    for(j=0;j<m;j++)
+    {
+        printf("\nmarks of %d subject: %f",j+1,(s+i)->ma.marks[j]);
+    }
+    }
+    a=n*sizeof(e);
+    printf("\n\nsize of structure after malloc: %d",a);
+    }
+    n++;
+    s=realloc(s,n*sizeof(struct std));
+    for(i=1;i<n;i++)
+    {
+    printf("\nenter id: ");
+    scanf("%d",&(s+i)->id);
+    printf("\nenter age: ");
+    scanf("%d",&(s+i)->age);
+    printf("\nenter name: ");
+    scanf("%s",(s+i)->name);
+    printf("\nenter the day-of-birth: ");
+    scanf("%d%d%d",&(s+i)->d.day,&(s+i)->d.month,&(s+i)->d.year);
+    for(j=0;j<m;j++)
+    {
+        printf("\nenter the marks of %d subject: ",j+1);
+        scanf("%f",&(s+i)->ma.marks[j]);
+    }
+    }
+    for(i=0;i<n;i++)
+    {
+    printf("\nid: %d",(s+i)->id);
+    printf("\nage: %d",(s+i)->age);
+    printf("\nname: %s",(s+i)->name);
+    printf("\nday-of-birth: %d-%d-%d",(s+i)->d.day,(s+i)->d.month,(s+i)->d.year);
+    for(j=0;j<m;j++)
+    {
+        printf("\nmarks of %d subject: %f\n",j+1,(s+i)->ma.marks[j]);
+    }
+    }
+    
+    a=n*sizeof(e);
+    printf("\n\nsize of structure after realloc: %d",a);
+    free(s);
+    printf("\n\nid: %d",s->id);
+    
+    }
