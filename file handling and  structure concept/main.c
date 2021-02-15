65e06cb3a483f9bfa07c9391e7d554216a2cc9cf
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-15 23:39:50 +0530

    file handling and structure concepts

--- file handling and  structure concept/main.c
+++ file handling and  structure concept/main.c
@@ -4,10 +4,16 @@
 struct std {
     int id,age;
     char name[100];
+    struct date_of_birth{
+        int day,month,year;
+    }d;
     float marks[100];
 }
+void structure_function(struct std *,int);
+void sum_of_marks(struct std *,int);
 void main()
 {
+    int n,m
     
     printf();
 }
