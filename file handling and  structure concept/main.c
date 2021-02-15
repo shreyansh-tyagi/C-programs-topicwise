663552777fcc922ba23fbcf1f8fbd36548495ae7
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-02-15 23:54:58 +0530

    linear search program has been added

--- file handling and  structure concept/main.c
+++ file handling and  structure concept/main.c
@@ -15,11 +15,12 @@ void structure_function(struct std *,int);
 void sum_of_marks(struct std *,int);
 void main()
 {
-    int n,m;
+    int n,m,i,j;
     void structure_function_pointer(struct std *,int);
     structure_function_pointer=&structure_function;
     void sum_of_marks_function_pointer(struct std * ,int);
     sum_of_marks_function_pointer=&sum_of_marks;
+    s=(struct std *)calloc(n,sizeof(struct std));
     printf("\nEnter the number of student: ");
     scanf("%d",&n);
     printf("\nEnter the number of subjects: ");
