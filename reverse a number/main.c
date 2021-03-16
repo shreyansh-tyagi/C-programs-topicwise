55bce966defe5c81cda3a979259378eb0d2930a0
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-16 22:01:53 +0530

    reverse a number has been added

---
+++ reverse a number/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void main()
+{
+    int i,n,a;
+    printf("enter the number to be reversed: \n");
+    scanf("%d",&n);
+    a=n;
+    while(a!=0)
+    {
+        
+    }
+
+}
