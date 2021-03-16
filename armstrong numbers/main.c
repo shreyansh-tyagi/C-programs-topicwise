59bc8322ae0a1584e7a87c892653a0c5152ec457
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-17 00:04:31 +0530

    aramstrong number

---
+++ armstrong numbers/main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void main()
+{
+    int n,a,b,len=0,sum=0;
+    printf("enter the number for armstrong number: \n");
+    scanf("%d",&n);
+    a=n;
+    while(n!=0)
+    {
+        n=n/10;
+        len++;
+    }
+    while(n!=0)
+    {
+        b=b%10;
+        
+    }
+
+}
