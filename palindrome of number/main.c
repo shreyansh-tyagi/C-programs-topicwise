42ec1977cd3c7db5e69ddc9cf21ab8c4976b9359
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-16 22:18:16 +0530

    palindrome of number

---
+++ palindrome of number/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void main()
+{
+    int i,n,a;
+    printf("enter the number to get the palindrome of number: \n");
+    scanf("%d",&n);
+    
+}
