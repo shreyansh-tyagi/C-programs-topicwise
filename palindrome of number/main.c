f46ddef9dad0b8c652c95c113bdee463c04815ad
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-03-16 22:51:38 +0530

    reverse a number has been added

--- palindrome of number/main.c
+++ palindrome of number/main.c
@@ -3,14 +3,29 @@
 #include<string.h>
 void main()
 {
-    int i,n,a,b;
+    int i,n,a,b,c[50],len=0,d;
     printf("enter the number to get the palindrome of number: \n");
     scanf("%d",&n);
     a=n;
     while(a!=0)
     {
+        a=a/10;
+        len++;
+    }
+    while(a!=10)
+    {
+        b=b%10;
+        a=a/10;
+    }
+    while(a!=0)
+    {
         b=a%10;
         a=a/10;
+        for(i=0;i<len;i++)
+        {
+            c[i]=b;
+        }
+
 
     }
 }
