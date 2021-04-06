19589f2d25ec9686cb01161fba0d69fed546650b
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-04-07 00:01:14 +0530

    transpose of matrix has been added

---
+++ 2d array print/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+    int a[2][2], b[2][2];
+    int i, j;
+    // printf("Enter here :-");
+    printf("\nMatrix-A\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("\nMatrix-B\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+    printf("\nPRINT\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf(" %d", a[i][j]);
+        }
+        printf("\n");
+    }
+      
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf(" %d", b[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
