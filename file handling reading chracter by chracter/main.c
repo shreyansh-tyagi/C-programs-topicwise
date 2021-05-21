19b4eed6cb5294cd2a3e8c5b5311537ae869f9db
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-21 22:57:01 +0530

    file handling that can read character by character

---
+++ file handling reading chracter by chracter/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void main()
+{
+    FILE *ptr;
+    ptr=fopen("D://c programs//file handling reading chracter by chracter//test.txt","r");
+}
