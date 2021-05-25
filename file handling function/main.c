938abc0c015e56f38644593342859e4c4c90cc7b
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-25 22:00:05 +0530

    file handling function

--- file handling function/main.c
+++ file handling function/main.c
@@ -4,6 +4,9 @@
 void main()
 {
     FILE *ptr=NULL;
-    char s[50];
+    char s[80]="this is the text that we can put into test file";
     ptr=fopen("test.txt","w");
+    for(int i=0;i<strlen(s);i++)
+    fputc(s[i],ptr);
+
 }
