08534a3b1fdb9bacd629269d78ad2e5ec1b4b892
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-25 22:04:09 +0530

    file handling function

--- file handling function/main.c
+++ file handling function/main.c
@@ -5,8 +5,11 @@ void main()
 {
     FILE *ptr=NULL;
     char s[80]="this is the text that we can put into test file";
-    ptr=fopen("test.txt","w");
+    ptr=fopen("test.txt","a"); //opening in append mode
     for(int i=0;i<strlen(s);i++)
-    fputc(s[i],ptr);
+    fputc(s[i],ptr);   // this fputc gives the one character to the test file at a time here it is inside the loop start 
+    // from zero to the length of string using strlen function
+    fprintf(ptr,"\n\nfputc function: ");
+    fputc(s[0],ptr);
 
 }
