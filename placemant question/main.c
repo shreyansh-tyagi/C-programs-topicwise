7354a6bc59150c20381fb45474b9df35074719bd
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-31 22:50:34 +0530

    placement question in c

--- placemant question/main.c
+++ placemant question/main.c
@@ -5,10 +5,15 @@ int main()
     printf("%d",var--);
     if(var)
     main(); */
-    int a =320;
+   /* int a =320;
     char *ptr;
     ptr=(char *)&a;
     printf("%d",*ptr);
-    return 0;
+    return 0;*/
+   /* int a=0;
+    while(a++);
+    {
+    printf("heloo");
+    }*/
 }
 
